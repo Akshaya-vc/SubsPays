995b9c79898d5eeb7c9ad1beb3fe037f4f26e787
Akshaya Chelliah <72297207+Akshaya-vc@users.noreply.github.com>
2021-05-05 17:33:15 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("card").style.display = "block";
+    console.log("workded")
+
+}
+
+function closeNav() {
+    document.getElementById("card").style.width = "0";
+}
